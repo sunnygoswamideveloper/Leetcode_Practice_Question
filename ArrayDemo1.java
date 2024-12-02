e441aee17ecb2d4d4de2850cdde3ca74f66f6f26
sunnygoswami <mr.sunnygoswami@gmail.com>
2024-12-02 16:49:12 +0530

    test_commit

---
+++ ArrayDemo1.java
@@ -0,0 +1,11 @@
+import com.sun.jdi.PathSearchingVirtualMachine;
+
+public class ArrayDemo1 {
+
+    public static void main(String[] args) {
+        int i=20;
+        int j=4;
+
+        System.out.println( i+j);
+    }
+}
